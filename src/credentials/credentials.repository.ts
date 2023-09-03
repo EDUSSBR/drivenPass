4ef613fb94c1be85b8d871d191fe34e7bd36ca01
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-03 14:41:48 -0300

    chore: add credential repository

---
+++ src/credentials/credentials.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCredentialDto } from './dto/create-credential.dto';
+import { UpdateCredentialDto } from './dto/update-credential.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CredentialsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createCredentialDto: CreateCredentialDto, userId: number) {
+    const { title, url, username, password } = createCredentialDto;
+    return this.prisma.credentials.create({
+      data: { title, url, username, password, userId },
+    });
+  }
+
+  findAll(userId) {
+    return this.prisma.credentials.findMany({ where: { userId } });
+  }
+
+  findOne(id, userId) {
+    return this.prisma.credentials.findFirst({ where: { userId, id } });
+  }
+
+  update(id: number, updateCredentialDto: UpdateCredentialDto, userId: number) {
+    const { title, url, username, password } = updateCredentialDto;
+    return this.prisma.credentials.update({
+      where: { userId, id },
+      data: { title, url, username, password },
+    });
+  }
+
+  remove(id: number, userId: number) {
+    return this.prisma.credentials.delete({ where: { userId, id } });
+  }
+}
