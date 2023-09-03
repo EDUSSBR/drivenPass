70ddd28474276c1b2b7ca6bfaeefe319e1f6a080
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-03 14:42:27 -0300

    chore: add encrypt and decrypt method into Encrypter Service

--- src/encrypter/encrypter.service.ts
+++ src/encrypter/encrypter.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import Cryptr from 'cryptr';
+
 @Injectable()
 export class EncrypterService {
-  saltRounds: number = 10;
+  private readonly saltRounds: number = 10;
+  private readonly secret: string = process.env.ENCRYPT_SECRET;
   hash(str: string) {
     return bcrypt.hashSync(str, this.saltRounds);
   }
   compare(pass, str) {
     return bcrypt.compareSync(pass, str);
+  }
+  encrypt(str: string) {
+    const encripter = new Cryptr(this.secret);
+    return encripter.encrypt(str);
+  }
+  decrypt(str: string) {
+    const encripter = new Cryptr(this.secret);
+    return encripter.decrypt(str);
   }
 }
