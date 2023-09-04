7cd644254fd5e8f71b84df623c3f8a68603f363a
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-04 14:42:42 -0300

    feat: add documentation for credentials

--- src/credentials/credentials.controller.ts
+++ src/credentials/credentials.controller.ts
@@ -9,17 +9,38 @@ import {
   UseGuards,
   ConflictException,
   InternalServerErrorException,
+  HttpStatus,
 } from '@nestjs/common';
 import { CredentialsService } from './credentials.service';
 import { CreateCredentialDto } from './dto/create-credential.dto';
 import { UpdateCredentialDto } from './dto/update-credential.dto';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { User } from 'src/decorators/users.decorator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Credentials')
+@ApiBearerAuth()
 @Controller('credentials')
 export class CredentialsController {
   constructor(private readonly credentialsService: CredentialsService) {}
-
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'A credential was sucessfully created',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'The credential is with invalid body or missing properties',
+  })
+  @ApiOperation({ summary: 'create a new credential' })
   @UseGuards(AuthGuard)
   @Post()
   async create(
@@ -40,18 +61,54 @@ export class CredentialsController {
     return 'CREATED';
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User should receive all their credentials',
+  })
+  @ApiOperation({ summary: 'get all users credential' })
   @UseGuards(AuthGuard)
   @Get()
   async findAll(@User() userId: number) {
     return await this.credentialsService.findAll(userId);
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'User should receive a specified credential if it belongs to the user',
+  })
+  @ApiOperation({ summary: 'get a specified credential' })
   @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string, @User() userId: number) {
     return await this.credentialsService.findOne(+id, userId);
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Updated credential sucessfully',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Cannot found this credential',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'This credential does not belong to the user',
+  })
+  @ApiOperation({ summary: 'update a specified credential' })
   @UseGuards(AuthGuard)
   @Patch(':id')
   async update(
@@ -62,6 +119,23 @@ export class CredentialsController {
     return this.credentialsService.update(+id, updateCredentialDto, userId);
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Credential was deleted sucessfully',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Cannot found this credential',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'This credential does not belong to the user',
+  })
+  @ApiOperation({ summary: 'delete a credential' })
   @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string, @User() userId: number) {
--- src/credentials/dto/create-credential.dto.ts
+++ src/credentials/dto/create-credential.dto.ts
@@ -1,16 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class CreateCredentialDto {
+  @ApiProperty({
+    example: 'Facebook password',
+    description: 'Name/Title for this credential',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
+  @ApiProperty({
+    example: 'hehe@gmail.com',
+    description: 'Your username/email or whatever you can use to log in',
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
+  @ApiProperty({
+    example: 'www.facebook.com',
+    description: 'Url where you should use this password',
+  })
   @IsString()
   @IsNotEmpty()
   @IsUrl({})
   url: string;
+  @ApiProperty({
+    example: 'mysecretpassword',
+    description: 'Your password to make login',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
