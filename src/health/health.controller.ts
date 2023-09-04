532a48c8ca3aa6dfd8b69128e8f86c5f1b29334f
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-04 14:43:37 -0300

    feat: add health docs

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('Health')
 @Controller('health')
 export class HealthController {
+  @ApiOperation({
+    summary: 'It was made only for testing if api is up and running',
+  })
   @Get()
   getHealth() {
     return "I'm Okay";
