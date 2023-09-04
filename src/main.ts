16cb9fd3124c1935d3e8b59ac31cc53aa5993b56
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-04 14:43:03 -0300

    build: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('DrivenPass')
+    .setDescription(
+      'Now you dont need to remember all your passwords anymore, this api should be used for keeping your secrets secret!!! You can guard all your usefull notes, cards, passwords and etc! enjoy!',
+    )
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
