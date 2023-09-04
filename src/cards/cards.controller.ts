a0cc33cd6fa71fe79341d69a3bd5ee0568ff6399
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-04 14:42:01 -0300

    feat: add documentation for cards

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -11,18 +11,38 @@ import {
   InternalServerErrorException,
   NotFoundException,
   ForbiddenException,
+  HttpStatus,
 } from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { User } from 'src/decorators/users.decorator';
 import { CardsService } from './cards.service';
-
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+@ApiTags('Cards')
+@ApiBearerAuth()
 @UseGuards(AuthGuard)
 @Controller('cards')
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
-
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'A card was sucessfully created',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'The card is with invalid body or missing properties',
+  })
+  @ApiOperation({ summary: 'Create a card' })
   @Post()
   async create(@Body() createCardDto: CreateCardDto, @User() userId: number) {
     try {
@@ -39,16 +59,52 @@ export class CardsController {
     return 'CREATED';
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User should receive all their cards',
+  })
+  @ApiOperation({ summary: "Get all user's cards" })
   @Get()
   findAll(@User() userId: number) {
     return this.cardsService.findAll(userId);
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'User should receive a specified card if it belongs to the user',
+  })
+  @ApiOperation({ summary: 'Get a specified user card' })
   @Get(':id')
   findOne(@Param('id') id: string, @User() userId: number) {
     return this.cardsService.findOne(+id, userId);
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Updated card sucessfully',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Cannot found this card',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'This card does not belong to the user',
+  })
+  @ApiOperation({ summary: 'Update a card info' })
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -78,6 +134,23 @@ export class CardsController {
     return updated;
   }
 
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not send a valid token',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Card was deleted sucessfully',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Cannot found this card',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'This card does not belong to the user',
+  })
+  @ApiOperation({ summary: 'delete a specified card' })
   @Delete(':id')
   remove(@Param('id') id: string, @User() userId: number) {
     return this.cardsService.remove(+id, userId);
--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -1,26 +1,59 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CardType } from '@prisma/client';
 import { IsDateString, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateCardDto {
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Master Do BB',
+    description: 'A name/title for all card info',
+  })
   title: string;
+  @ApiProperty({
+    example: 'Eduardo S Santos',
+    description: 'The name written in the card',
+  })
   @IsNotEmpty()
   name: string;
+  @ApiProperty({
+    example: '4010110022003900',
+    description: 'Cards number',
+  })
   @IsNotEmpty()
   @IsString()
   @Length(16)
   number: string;
+  @ApiProperty({
+    example: '074',
+    description: 'CCV - number behind the card',
+  })
   @IsNotEmpty()
   @IsString()
   @Length(3)
   ccv: string;
+  @ApiProperty({
+    example: '12-34',
+    description: 'Date when your card expires',
+  })
   @IsNotEmpty()
   @IsDateString()
   expirationDate: Date;
+  @ApiProperty({
+    example: '123456',
+    description: 'Your card Password should be here!',
+  })
   @IsNotEmpty()
   password: string;
+  @ApiProperty({
+    example: true,
+    description: "Says if it's a virtual card or not (true/false)",
+  })
   @IsNotEmpty()
   isVirtual: boolean;
+  @ApiProperty({
+    example: 'DEBIT',
+    description: 'Says how should be used the card (DEBIT/CREDIT/BOTH)',
+  })
   @IsNotEmpty()
   type: CardType;
 }
