facc0d576f4a7bf9e9b1b0dd3ef79808f8ccd88e
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-09-04 14:44:21 -0300

    feat: add documentation for user/account

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'myemail@gmail.com',
+    description: 'users email for registration',
+  })
   @IsEmail()
   email: string;
+  @ApiProperty({
+    example: 'Mysecretpassword%###!789',
+    description: 'users password for registration',
+  })
   @IsStrongPassword({
     minLength: 10,
     minLowercase: 1,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,28 +5,80 @@ import {
   Delete,
   UseGuards,
   Param,
+  HttpStatus,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { User } from 'src/decorators/users.decorator';
 import { RemoveUserDto } from './dto/remove-user.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
-@Controller('users')
+@ApiTags('Users')
+@Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post('/signup')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'User was creted',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: "incorrect body or properties didn't met criterias",
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'An account with same e-mail already exists ',
+  })
+  @ApiOperation({ summary: 'create a user' })
+  @Post('/users/signup')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.createAccount(createUserDto);
   }
 
-  @Post('/signin')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'Sucess, you should receive a token because all informations are valid',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: "incorrect body or properties didn't met criterias",
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Invalid password',
+  })
+  @ApiOperation({ summary: 'make user signin' })
+  @Post('/users/signin')
   verifyUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.verifyAccount(createUserDto);
   }
+
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'User does not own account or send an invalid password',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User does not exists',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description:
+      'User exists but have no authorization for doing it (trying deletion of other account)',
+  })
+  @ApiOperation({
+    summary: 'remove an user account and all info attached to it',
+  })
+  @ApiBearerAuth()
   @UseGuards(AuthGuard)
-  @Delete('/:id')
+  @Delete('/erase/:id')
   deleteUser(
     @Body() removeUserDto: RemoveUserDto,
     @Param('id') id: string,
